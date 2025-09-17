da4ecbe3b4afeac0f4e9bc3cf860ee54f352f766
Luke Perron <LukeDPerron@users.noreply.github.com>
2025-09-16 21:46:35 -0400

    added print to pull sword

--- story.py
+++ story.py
@@ -12,6 +12,7 @@ def intro():
 
 def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
+    print("You grab the shaft of the sword, pulling with all your might.")
 
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
